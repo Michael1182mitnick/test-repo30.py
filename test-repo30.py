cdc676dc41a65468d7d847956c74785a4efbad42
Michael1182mitnick <56063336+Michael1182mitnick@users.noreply.github.com>
2024-07-21 20:01:40 -0800

    Create test-repo30.py

---
+++ test-repo30.py
@@ -0,0 +1,8 @@
+# write a program that counts how many even number
+# using for loop 
+count = 0
+for number in range(1, 10):
+    if number % 2 == 0:
+        count += 1
+        print(number)
+print(f"We have {count} even numbers")
